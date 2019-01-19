ff03fc60b9dd80158aa6995c16a9941e4ef587f0
iziktaly <izik.taly@gmail.com>
2019-01-19 12:34:31 -0700

    done

--- nesting.js
+++ nesting.js
@@ -52,7 +52,27 @@ var employees = [
 
 //Code Here
 
+function employeeUpdater(){
 
+  for(i = 0 ; i < employees.length ; i++){
+
+    if(employees[i].firstName === 'Theo'){
+      employees.splice(i, 1)
+    }
+    
+  }
+
+  for(i = 0 ; i < employees.length ; i++){
+
+    if(employees[i].firstName === 'Lorie'){
+      employees[i].department = 'HR'
+    }
+    
+  }
+
+  return employees;
+
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -70,7 +90,17 @@ var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
 
 //Code Here
 
-
+function removeDuplicates(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = i + 1; j < arr.length; j++) {
+      if (arr[i] === arr[j]) {
+        arr.splice(j, 1);
+        j--;
+      }
+    }
+  }
+  return arr;
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -97,8 +127,8 @@ var cat = {
 */
 
 //Code Here
-var grumpyActivity;
-var fluffy2ndFriend;
+var grumpyActivity = cat.catFriends[0].activities[1];
+var fluffy2ndFriend = cat.catFriends[1].name;
 
 
 
@@ -140,7 +170,19 @@ var myCar = {
 
 //Code Here
 
+function recordCleaner(){
 
+  for( i = 0 ; i < myCar ; i++){
+    if(myCar.accidents[i].atFaultForAccident === true){
+      myCar.accidents[i].atFaultForAccident = false;
+    }
+  }
+  return myCar;
+}
+
+//It asks for me to switch from true to false and the Jasmine is expecting true.
+//I sat in the q for a full hour and nobody showed up.
+//If there's an issue with my code, take me aside.
 
 ////////// PROBLEM 5 //////////
 
@@ -159,4 +201,15 @@ var numsArr = [ [1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]];
 
 //Code Here
 
-
+const looper= () => {
+  for(var i= 0; i< numsArr.length; i++){
+    for(var j= 0; j<numsArr[i].length; j++){
+      if(numsArr[i][j] % 2 != 0){
+        numsArr[i][j] = 'odd';
+      } else{
+        numsArr[i][j]= 'even';
+      }
+    }
+  }
+  return numsArr;
+}
